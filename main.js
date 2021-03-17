cee7af428e24cddffb2a65247806b1c1e6f577d8
alecteucilide <alecadrianteucilide@gmail.com>
2021-03-17 18:55:20 +0100

    endpoint with params added

--- main.js
+++ main.js
@@ -39,7 +39,8 @@ app.get("/",(request,response) => {
     response.json("Server works!");
 });
 
-//login student -> getStudent + generate token and save it
+//generating ajax request with body -> https://stackoverflow.com/questions/4159701/jquery-posting-valid-json-in-request-body
+//login student con body-> getStudent + generate token and save it
 app.get("/login/student", (request, response) => {
     const {email, password} = request.body;
     database.collection('students').findOne({email: email, password: password}, (error, student) => {
@@ -64,7 +65,59 @@ app.get("/login/student", (request, response) => {
     });
 });
 
+//login student con body-> getStudent + generate token and save it
+app.get("/login/student/:email/:password", (request, response) => {
+    const email = request.params.email;
+    const password = request.params.password;
+    database.collection('students').findOne({email: email, password: password}, (error, student) => {
+        if(error){
+            return response.status(500).json({error: error.message});
+        }
+        if(student == null){
+            response.status(200).json({msg: "Authentification failed."})
+        }else{
+            const {_id, name} = student;
+            const token = jwt.sign({ name: name }, email);
+            
+            database.collection('students').updateOne({
+                "_id": new objectId(_id)
+            }, {
+                $set: {
+                    token: token
+                }
+            });
+            response.status(200).json(student);
+        }
+    });
+});
+
 //login admin -> getAdmin + generate token and save it
+app.get("/login/admin/:email/:password", (request, response) => {
+    const email = request.params.email;
+    const password = request.params.password;
+    database.collection('admins').findOne({email: email, password: password}, (error, admin) => {
+        if(error){
+            return response.status(500).json({error: error.message});
+        }
+        if(admin == null){
+            response.status(200).json({msg: "Authentification failed."})
+        }else{
+            const {_id, name} = admin;
+            const token = jwt.sign({ name: name }, email);
+            
+            database.collection('admins').updateOne({
+                "_id": new objectId(_id)
+            }, {
+                $set: {
+                    token: token
+                }
+            });
+            response.status(200).json(admin);
+        }
+    });
+});
+
+//login admin with body -> getAdmin + generate token and save it
 app.get("/login/admin", (request, response) => {
     const {email, password} = request.body;
     database.collection('admins').findOne({email: email, password: password}, (error, admin) => {
